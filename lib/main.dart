52b8cfc970ecf41b0923fd29da25fabd6a94698c
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-12-07 23:08:10 +0330

    Integrated OpenWeatherMap API to fetch weather data based on user location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:clima/screens/home.dart';
+import 'screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:climate/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,14 +11,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  GeolocatorPlatform geolocatorPlatform = GeolocatorPlatform.instance;
   LocationPermission? permission;
 
+  void getData() async{
+    http.Response response = await http.get(Uri.parse("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=078c4f6b9c9b3dba5ed8c615f16b30f7"));
+      if(response.statusCode == 200){
+        String data = response.body;
+        print(data);
+      }else{
+        print(response.statusCode);
+      };
+  }
   Future<void> getPermission() async {
-    permission = await geolocatorPlatform.checkPermission();
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      // Request permission
-      permission = await geolocatorPlatform.requestPermission();
+      permission = await Geolocator.requestPermission();
       if (permission != LocationPermission.denied) {
         if (permission == LocationPermission.deniedForever) {
           print(
@@ -34,24 +43,22 @@ class _HomeState extends State<Home> {
   }
 
   Future<void> getLocation() async {
-    final LocationSettings locationSettings = const LocationSettings(
-      accuracy: LocationAccuracy.low,
-      distanceFilter: 1000,
-    );
-    Position position =
-        await Geolocator.getCurrentPosition(locationSettings: locationSettings);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print('Latitude: ${location.latitude}');
+    print('Longitude: ${location.longitude}');
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getPermission();
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: SafeArea(
         child: Center(
