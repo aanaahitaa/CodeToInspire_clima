be1e538a44115c9a2dc3e98262983774733b7d59
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-12-15 00:10:57 +0330

    Updated UI with Google Fonts, added loading spinner with Flutter Spinkit.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Climate Weather App',
       theme: ThemeData.dark(),
       home: const Home(),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,11 @@
+import 'package:climate/Components/loading_widget.dart';
 import 'package:climate/services/networking.dart';
 import 'package:climate/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:climate/services/location.dart';
+
+import '../Components/details_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +15,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isDataLoaded = false;
   double? latitude, longitude;
   GeolocatorPlatform geolocatorPlatform = GeolocatorPlatform.instance;
   LocationPermission? permission;
@@ -45,6 +49,9 @@ class _HomeState extends State<Home> {
     NetworkHelper networkHelper = NetworkHelper(
         "http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
     var weatherData = await networkHelper.getData();
+    setState(() {
+      isDataLoaded = true;
+    });
   }
 
   @override
@@ -55,21 +62,122 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              getLocation();
-            },
-            child: const Text(
-              'Get Location',
-              style: TextStyle(fontSize: 16),
-            ),
+    if (!isDataLoaded) {
+      return const LoadingWidget();
+    } else {
+      return Scaffold(
+        backgroundColor: kOverlayColor,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: TextField(
+                        decoration: kTextFieldDecoration,
+                        onSubmitted: (String typedName) {
+                          print(typedName);
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white12,
+                          elevation: 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Container(
+                          height: 60,
+                          child: Row(
+                            children: const [
+                              Text('My Location', style: kTextFieldTextStyle),
+                              SizedBox(width: 8),
+                              Icon(Icons.gps_fixed, color: Colors.white60),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.location_city, color: kMidLightColor),
+                        SizedBox(width: 12),
+                        Text(
+                          'City Name',
+                          style: kLocationTextStyle,
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    Icon(Icons.wb_sunny_outlined, size: 280),
+                    SizedBox(height: 40),
+                    Text('00°', style: kTempTextStyle),
+                    Text('CLEAR SKY', style: kLocationTextStyle),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  color: kOverlayColor,
+                  child: Container(
+                    height: 90,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        DetailsWidget(
+                          title: 'FEELS LIKE',
+                          value: '31°',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: VerticalDivider(thickness: 1),
+                        ),
+                        DetailsWidget(
+                          title: 'HUMIDITY',
+                          value: '15%',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: VerticalDivider(thickness: 1),
+                        ),
+                        DetailsWidget(
+                          title: 'WIND',
+                          value: '7',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
 
